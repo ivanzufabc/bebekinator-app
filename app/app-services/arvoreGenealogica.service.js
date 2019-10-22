3cfa11ff8ca9d757d34c3fc133a4c64e8b9247f5
ivanzufabc <ivan.z@aluno.ufabc.edu.br>
2019-10-22 13:34:01 -0300

    arquitetura

---
+++ app/app-services/arvoreGenealogica.service.js
@@ -0,0 +1,42 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('arvoresService', Service);
+
+    function Service($http, $q) {
+        var service = {};
+
+        service.Create = Create;
+        service.List = List;
+        service.Delete = Delete;
+        service.Update = Update;
+
+        return service;
+        function Create(arvore) {
+            return $http.post('/api/arvoreGenealogica/create', arvore).then(handleSuccess, handleError);
+        }
+
+        function List(perguntas) {
+            return $http.get('/api/arvoreGenealogica/list', perguntas).then(handleSuccess, handleError);
+        }
+
+        function Delete(_id) {
+            return $http.delete('/api/arvoreGenealogica/' + _id).then(handleSuccess, handleError);
+        }
+
+        function Update(arvore) {
+            return $http.put('/api/arvoreGenealogica/' + arvore._id, arvore).then(handleSuccess, handleError);
+        }
+
+        function handleSuccess(res) {
+            return res.data;
+        }
+
+        function handleError(res) {
+            return $q.reject(res.data);
+        }
+    }
+
+})();
